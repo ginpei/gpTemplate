bff4ef5e434a875ad9e9c1ea4a6c48c4eb45ea47
Takanashi Ginpei <ginpei@gmail.com>
2012-09-04 02:02:22 +0900

    replace codes that using RegExp with using `.indexOf()`

--- gptemplate.js
+++ gptemplate.js
@@ -1,10 +1,54 @@
 (function() {
 	var NS = window.ginpencom = window.ginpencom || {};
 	NS.template = function(source, data) {
-			return document.querySelector(source).text
-				.replace(/{([?#$]?)(\w+)}/g, function(all, operation, key) {
+		var rx = /[a-zA-Z]/;
+		var templateText = document.querySelector(source).text
+		var result = '';
+		var end = 0;
+
+		while (end >= 0) {
+			var start = templateText.indexOf('{', end) + 1;
+			if (start < 1) {
+				break;
+			}
+
+			result += templateText.slice(end, start-1);
+
+			end = templateText.indexOf('}', start);
+			if (end < 1) {
+				break;
+			}
+
+			var inner = templateText.slice(start, end);
+			var first = inner[0];
+			var key = rx.test(first)?inner:inner.slice(1);
+			var value = (key in data ? data[key] : '');
+
+			if (first == '$') {
+				result += value;
+			}
+			else {
+				result += ('' + value)
+					.replace(/&/g, '&amp;')
+					.replace(/</g, '&lt;')
+					.replace(/>/g, '&gt;')
+					.replace(/"/g, '&quot;')
+					.replace(/'/g, '&#x27;');
+			}
+
+			end = end + 1;
+		}
+
+		result += templateText.slice(end);
+
+		return result;
+
+	
+
+	/*
+				.replace(/{([?#$:\/]?)(\w+)}/g, function(all, operation, key) {
 					var hasData = (key in data);
-					var value = (hasData ? data[key] : '');
+					var value = '' + (hasData ? data[key] : '');
 					if (operation == '$') {
 						return value;
 					}
@@ -17,5 +61,6 @@
 							.replace(/'/g, '&#x27;');
 					}
 				});
+				*/
 	}
 })();
